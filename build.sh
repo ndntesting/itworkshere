952629d8ba7224b2612cf97ade8053e656c281b0
ndntesting <ndntesting@outlook.com>
2018-06-06 22:06:53 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/itworkshere/*.csproj && dotnet build src/itworkshere/*.csproj
---
+++ src/itworkshere/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace itworkshere
+{
+    public class Class1
+    {
+    }
+}
